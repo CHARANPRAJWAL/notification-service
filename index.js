d980318a42ef077507d5499b6ea828a13e743218
charan <charanprajwal@Charans-MacBook-Pro.local>
2023-10-05 18:04:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/notify", (req, res) => {
+  res.send("notify");
+});
+app.listen("4002", () => {
+  console.log("SERVER STARTED ");
+});
